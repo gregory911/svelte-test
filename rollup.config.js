f7ef6b053c929d97bc2c63de084a8d80d1dd607b
Gregory Gegoux <gregory.gegoux@gmail.com>
2021-11-25 01:15:45 -0500

    Clean template;

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,7 @@ export default {
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
 				// enable run-time checks when not in production
+				sourcemap: true,
 				dev: !production
 			}
 		}),
